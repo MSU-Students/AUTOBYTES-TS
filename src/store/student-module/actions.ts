e09938c9069d361a5e5c4732d42e0344dfe06c14
usman12 <usman12strygwyr@gmail.com>
2021-07-14 13:13:46 +0800

    student module

--- src/store/student-module/actions.ts
+++ src/store/student-module/actions.ts
@@ -1,16 +1,17 @@
 import { ActionTree } from 'vuex';
 import { StateInterface } from '../index';
 import { StudentStateInterface } from './state';
-import studentService from 'src/services/Student.service';
+import helperService from 'src/services/helper.service';
+import studentService from 'src/services/student.service';
 
 const actions: ActionTree<StudentStateInterface, StateInterface> = {
-  async addStudent(context, payload: any) {
-    const result = await studentService.addUser(payload);
-    context.commit('addStudent', result);
+  async addStudent(context, file: File) {
+    const result = await helperService.uploadContacts(file);
+    context.commit('addStudents', result);
   },
-  async getStudent(context): Promise<any> {
-    const result = await studentService.getUser();
-    context.commit('getStudent', result);
+  async getStudents(context): Promise<any> {
+    const result = await studentService.getUsers();
+    context.commit('viewStudents', result);
   }
 };
 
--- src/store/ui-module/mutations.ts
+++ src/store/ui-module/mutations.ts
@@ -4,6 +4,25 @@ import { UiStateInterface } from './state';
 const mutation: MutationTree<UiStateInterface> = {
   leftDrawer(state, opened) {
     state.leftDrawerState = opened;
+  },
+  showAccountDialog(state, payload: boolean) {
+    state.showUser = payload;
+  },
+  showBulletinDialog(state, payload: boolean) {
+    console.log(payload)
+    state.showBulletin = payload;
+  },
+  showClearanceDialog(state, payload: boolean) {
+    state.showClearance = payload;
+  },
+  showAttendaceDialog(state, payload: boolean) {
+    state.showAttendance = payload;
+  },
+  showRecordsDialog(state, payload: boolean) {
+    state.showRecords = payload;
+  },
+  showArchivedDialog(state, payload: boolean) {
+    state.showRecords = payload;
   }
 };
 
