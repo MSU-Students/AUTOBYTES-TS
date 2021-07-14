88222635c57f6ff3dcbc30824ff74e125e5b9227
usman12 <usman12strygwyr@gmail.com>
2021-07-14 13:14:13 +0800

    student module

--- src/store/student-module/mutations.ts
+++ src/store/student-module/mutations.ts
@@ -2,12 +2,12 @@ import { MutationTree } from 'vuex';
 import { StudentStateInterface } from './state';
 
 const mutation: MutationTree<StudentStateInterface> = {
-  addStudent(state, result) {
-    console.log(result);
-    state.student.push(result.data);
+  addStudents(state, students) {
+    state.students.push(...students);
   },
-  getStudent(state, result) {
-    state.student.push(...result);
+  viewStudents(state, result) {
+
+    state.students = result;
   }
 };
 
